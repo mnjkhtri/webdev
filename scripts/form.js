09983d03994ca3d25bb6bddb0b8d31d0ce3a1f93
argmike <mike@108capital.ltd>
2024-05-25 22:40:42 +0545

    Reorganize

--- scripts/form.js
+++
@@ -1,7 +0,0 @@
-//Form example:
-const form = document.querySelector("form");
-const feedback_output = document.getElementById("feedback-output");
-form.addEventListener("submit", (e) => {
-    e.preventDefault(); // Prevents the default form submission behavior
-    feedback_output.textContent = "Thank you for submitting."
-})
